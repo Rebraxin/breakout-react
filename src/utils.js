f40f5b986c5032e48651d7e79f36381b419b2981
Cedric <cpaje@debutant-e.pro>
2021-01-30 08:49:02 +0100

    first commit

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export const getRange = (length) => [...Array(length).keys()]
+
+export const getRandomFrom = (...args) =>
+  args[Math.floor(Math.random() * args.length)]
+
+export const flatten = (arrays) =>
+  arrays.reduce((acc, row) => [...acc, ...row], [])
